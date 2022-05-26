1013b3aebb83058aeaa49b0490cf754200389c4d
olivervilla <olivervilla3096@gmail.com>
2022-05-26 15:40:38 -0500

    delete tutorial module

--- aa_module/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    "name": "My Module",
-    "description": "This is a facade.",
-    "data": [
-        "views.xml",
-        "security.xml"
-    ]
-}
--- aa_module/models.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class Todo(models.Model):
-    _name = "test.todo"
-    _description = 'Abstract Recurring Contract Line'
-
-    name = fields.Char("Name")
-    status = fields.Selection(selection=[("draft", "Draft"), ("done", "Done")])
